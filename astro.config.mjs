581c4ceacd7d4cdca7a22b93b9a6a2de2f66547c
zeta <unluckyzeta@gmail.com>
2026-06-25 14:40:26 +0300

    FieldCam Admin Portal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
 });
