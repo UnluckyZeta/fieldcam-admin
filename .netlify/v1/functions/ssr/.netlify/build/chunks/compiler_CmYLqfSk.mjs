cb81370e4efd2abd0ba5d8e63a704bc763627523
zeta <unluckyzeta@gmail.com>
2026-06-25 15:26:41 +0300

    railway deploy

--- .netlify/v1/functions/ssr/.netlify/build/chunks/compiler_CmYLqfSk.mjs
+++
@@ -1,2 +0,0 @@
-import "./server_DopqXNux.mjs";
-export {};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
